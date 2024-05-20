29631a4ba2db80c9a1d28ad433d5d20950cdba10
jiyoon-lee <jiwoonlee@bridgetec.co.kr>
2024-05-20 16:27:50 +0900

    :art: update build setting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Noto_Sans_KR } from "next/font/google";
+// import { Inter } from "next/font/google";
 import "./globals.css";
 
-const noto = Noto_Sans_KR({ weight: "400", subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +16,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={noto.className}>{children}</body>
+      {/* <body className={inter.className}>{children}</body> */}
+      <body>{children}</body>
     </html>
   );
 }
