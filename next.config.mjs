427d441d5751105202cd85e81b48d5036d1473ca
jiyoon-lee <jiwoonlee@bridgetec.co.kr>
2024-06-19 10:54:38 +0900

    remove output

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   assetPrefix: ".",
   pageExtensions: ["mdx", "md", "jsx", "js", "tsx", "ts"],
   async rewrites() {
-    if (process.env.NEXT_PUBLIC_EXPORT !== "true") {
-      return [
-        {
-          source: `${process.env.NEXT_PUBLIC_API_BASE_PATH}/:path*`,
-          destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/:path*`,
-        },
-      ];
-    }
-    return [];
+    return [
+      {
+        source: `${process.env.NEXT_PUBLIC_API_BASE_PATH}/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/:path*`,
+      },
+    ];
   },
 };
 
