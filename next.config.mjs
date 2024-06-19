1bf01c783caa5f474a8b6e308d26ebf16f68aa50
jiyoon-lee <jiwoonlee@bridgetec.co.kr>
2024-06-19 10:44:07 +0900

    NEXT_PUBLIC_EXPORT 추가

--- next.config.mjs
+++ next.config.mjs
@@ -4,12 +4,15 @@ const nextConfig = {
   assetPrefix: ".",
   pageExtensions: ["mdx", "md", "jsx", "js", "tsx", "ts"],
   async rewrites() {
-    return [
-      {
-        source: `${process.env.NEXT_PUBLIC_API_BASE_PATH}/:path*`,
-        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/:path*`,
-      },
-    ];
+    if (process.env.NEXT_PUBLIC_EXPORT !== "true") {
+      return [
+        {
+          source: `${process.env.NEXT_PUBLIC_API_BASE_PATH}/:path*`,
+          destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/:path*`,
+        },
+      ];
+    }
+    return [];
   },
 };
 
